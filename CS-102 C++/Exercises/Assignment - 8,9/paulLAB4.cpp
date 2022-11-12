523e70fc9eb1d3715d347fea69a98fea72451cfe
PaulStein1195 <pablerillas.11.pb@gmail.com>
2022-11-12 10:47:15 -0800

    updating final code of teh Assignment 4

--- CS-102 C++/Exercises/Assignment - 8,9/paulLAB4.cpp
+++ CS-102 C++/Exercises/Assignment - 8,9/paulLAB4.cpp
@@ -44,23 +44,90 @@ void getScore(int& number)
 
 }
 
+// Function returning the lowest value from the diving scores 
+int findLowest(int score1, int score2, int score3, int score4, int score5)
+{
+   int lowestVal;
+   lowestVal = score1;
+   if(score1 > score2) 
+   {
+      lowestVal = score2;
+   }
+   if(score2 > score3) 
+   {
+      lowestVal = score3;
+   }
+   if(score3 > score4) {
+      lowestVal = score4;
+   }
+   if(score4 > score5) {
+      lowestVal = score5;
+   }
+   return lowestVal;
+}
+
+// Function calculating average and calling lowest value  
+void calcAverage(int score1, int score2, int score3, int score4, int score5) 
+{
+   // Defining variables
+   int lowestScore;
+   int totalValues = score1 + score2 + score3 + score4 + score5;
+   double average;
+
+   // Getting lowestScore calling function findLowest 
+   lowestScore = findLowest(score1, score2, score3, score4, score5);
+   // Calculate average score
+   average = static_cast<double>(totalValues- lowestScore) / 4;
+
+   cout << "After dropping the lowest score, " 
+   << lowestScore << ", the diving average score is " << average;
+}
+
 int main() 
 {
-    // Initialize input variables
-    int number;
+   // Initialize input variables
+   int number;
+   int score1, score2, score3, score4, score5;
 
-    // Program initializing
-    // # 1
-    for(int i; i <= 5; i++)
-    {
-        getScore(number);
-    }
+   // Program initializing
+   // For loop to iterate 5 times to every score
+   for(int i = 1; i <= 5; i++)
+   {
+      getScore(number);
+        
+      // Identifying each number with iteration and ssaving in score position
+      if (i == 1)
+      {
+         score1 = number;
+      }
+      if (i == 2)
+      {
+         score2 = number;
+      }
+      if (i == 3)
+      {
+         score3 = number;
+      }
+      if (i == 4)
+      {
+         score4 = number;
+      }
+      if (i == 5)
+      {
+         score5 = number;
+      }        
+       
+   }
+
+   // Function to calculate and display average and minimum value
+   calcAverage(score1, score2, score3, score4, score5);
+
 
     return 0;
 
 }
 
-
+   
 
 /*
 Enter a diving score: -1
